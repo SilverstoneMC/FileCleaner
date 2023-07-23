1c94ed36f0a2ff5f6b0cc3050b08221796d7baab
Jason H <51760372+JasonHorkles@users.noreply.github.com>
2023-07-23 12:32:21 -0600

    Change plugins to loggers

--- src/main/java/me/jasonhorkles/filecleaner/CleanFiles.java
+++ src/main/java/me/jasonhorkles/filecleaner/CleanFiles.java
@@ -1,15 +1,13 @@
 package me.jasonhorkles.filecleaner;
 
-import net.md_5.bungee.api.plugin.Plugin;
-import org.bukkit.plugin.java.JavaPlugin;
-
 import java.io.File;
 import java.util.*;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 public class CleanFiles {
 
-    public void CleanFilesTask(String folderName, JavaPlugin sPlugin, Plugin bPlugin, com.velocitypowered.api.plugin.Plugin vPlugin, int age, int count, long size) {
+    public void CleanFilesTask(String folderName, Logger logger, int age, int count, long size) {
         File folder = new File("." + folderName);
         try {
             //noinspection DataFlowIssue
@@ -23,7 +21,7 @@ public class CleanFiles {
                     long diff = new Date().getTime() - file.lastModified();
 
                     if (diff > (long) age * 24 * 60 * 60 * 1000) {
-                        deleteFile(sPlugin, bPlugin, file);
+                        deleteFile(logger, file);
                         filesToRemove.add(file);
                     }
                 }
@@ -34,7 +32,7 @@ public class CleanFiles {
                 ArrayList<File> filesToRemove = new ArrayList<>();
                 files.sort(Comparator.comparingLong(File::lastModified));
                 for (int x = 0; x < files.size() - count; x++) {
-                    deleteFile(sPlugin, bPlugin, files.get(x));
+                    deleteFile(logger, files.get(x));
                     filesToRemove.add(files.get(x));
                 }
                 files.removeAll(filesToRemove);
@@ -43,24 +41,17 @@ public class CleanFiles {
             if (size > -1) {
                 files.sort(Comparator.comparingLong(File::length));
                 for (File file : files)
-                    if (Math.round(file.length() / 1024.0) > (double) size)
-                        deleteFile(sPlugin, bPlugin, file);
+                    if (Math.round(file.length() / 1024.0) > (double) size) deleteFile(logger, file);
             }
         } catch (NullPointerException e) {
-            if (sPlugin == null) bPlugin.getLogger().severe(
-                "Couldn't find the folder \"" + folder.getPath() + "\"! Check to make sure it's spelled correctly and is actually a folder.");
-            else sPlugin.getLogger().severe(
+            logger.severe(
                 "Couldn't find the folder \"" + folder.getPath() + "\"! Check to make sure it's spelled correctly and is actually a folder.");
         }
     }
 
-    public void deleteFile(JavaPlugin sPlugin, Plugin bPlugin, File file) {
-        if (file.delete()) if (sPlugin == null)
-            bPlugin.getLogger().info("Successfully deleted file \"" + file.getPath() + "\"");
-        else sPlugin.getLogger().info("Successfully deleted file \"" + file.getPath() + "\"");
-        else if (sPlugin == null) bPlugin.getLogger()
-            .severe("Couldn't delete file \"" + file.getPath() + "\" - make sure it's not currently in use!");
-        else sPlugin.getLogger().severe(
-                "Couldn't delete file \"" + file.getPath() + "\" - make sure it's not currently in use!");
+    public void deleteFile(Logger logger, File file) {
+        if (file.delete()) logger.info("Successfully deleted file \"" + file.getPath() + "\"");
+        else logger.severe(
+            "Couldn't delete file \"" + file.getPath() + "\" - make sure it's not currently in use!");
     }
 }
